df124e4104592b77d74b9a021cfc4657bc38c4b7
supperduc <teentitan119@gmail.com>
2024-01-09 22:12:25 +0700

    add file html, css, js

---
+++ Lab/lab1/script.js
@@ -0,0 +1,7 @@
+let gkBayernMunich = 'Manuel Neuer';
+let fieldPlayersBayernMunich = ['Dayot Upamecano', 'Kim Min-jae', 'Benjamin Pavard','Alphonso Davies','Joshua Kimmich','Paul Wagner','Thomas Muller','Leroy Sane','Gabriel Vidovic','Harry Kane'];
+let bayernMunich = [gkBayernMunich, ...fieldPlayersBayernMunich];
+
+
+// let arr = {borrussiaDortmund, bayernMunich};
+console.log(bayernMunich);
